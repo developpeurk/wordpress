32a5cd8b1e5969e0dd61ba1e5e908a146ce6bcca
softengyas <y.lambarki@etu.enset-media.ac.ma>
2022-07-23 21:45:09 +0100

    displayed the posts

--- index.php
+++ index.php
@@ -2,69 +2,34 @@
 
 <div class="container">
     <div class="row">
-    <div class="col-sm-6">
-            <div class="main-post">
-                <h3 class="post-title">This is our main Post Title</h3>
-                <span class="post-author"><i class="fa-regular fa-user"></i> Yassine</span>
-                <span class="post-date"><i class="fa-regular fa-calendar"></i> 23/07/2022</span>
-                <span class="post-comments"><i class="fa-regular fa-comment"></i>  20 comments</span>
-                <img class="img-responsive img-thumbnail" src="https://source.unsplash.com/random/600x200/?img=1" alt="">
-                <p class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sed hic consectetur magnam omnis? Quae quo cupiditate facere, voluptatem praesentium cum labore, ipsam repellat culpa necessitatibus atque. Distinctio officia beatae atque, quasi ipsam blanditiis, neque provident dicta, quod veniam dignissimos autem cumque minus quae vel dolor illum voluptatibus iste aut?</p>
-                <hr>
-                <p class="categories">
-                <i class="fa-solid fa-tags"></i>                    
-                    Html, Test, Coding
-                </p>
-            </div>
-        </div>
-        <div class="col-sm-6">
-            <div class="main-post">
-                <h3 class="post-title">This is our main Post Title</h3>
-                <span class="post-author"><i class="fa-regular fa-user"></i> Yassine</span>
-                <span class="post-date"><i class="fa-regular fa-calendar"></i> 23/07/2022</span>
-                <span class="post-comments"><i class="fa-regular fa-comment"></i> 20 comments</span>
-                <img class="img-responsive img-thumbnail" src="https://source.unsplash.com/random/600x200/?img=3" alt="">
-                <p class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sed hic consectetur magnam omnis? Quae quo cupiditate facere, voluptatem praesentium cum labore, ipsam repellat culpa necessitatibus atque. Distinctio officia beatae atque, quasi ipsam blanditiis, neque provident dicta, quod veniam dignissimos autem cumque minus quae vel dolor illum voluptatibus iste aut?</p>
-                <hr>
-                <p class="categories">
-                <i class="fa-solid fa-tags"></i>                    
-                    Html, Test, Coding
-                </p>
-            </div>
-        </div>
-        <div class="col-sm-6">
-            <div class="main-post">
-                <h3 class="post-title">This is our main Post Title</h3>
-                <span class="post-author"><i class="fa-regular fa-user"></i> Yassine</span>
-                <span class="post-date"><i class="fa-regular fa-calendar"></i> 23/07/2022</span>
-                <span class="post-comments"><i class="fa-regular fa-comment"></i> 20 comments</span>
-                <img class="img-responsive img-thumbnail" src="https://source.unsplash.com/random/600x200/?img=5" alt="">
-                <p class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sed hic consectetur magnam omnis? Quae quo cupiditate facere, voluptatem praesentium cum labore, ipsam repellat culpa necessitatibus atque. Distinctio officia beatae atque, quasi ipsam blanditiis, neque provident dicta, quod veniam dignissimos autem cumque minus quae vel dolor illum voluptatibus iste aut?</p>
-                <hr>
-                <p class="categories">
-                <i class="fa-solid fa-tags"></i>                    
-                    Html, Test, Coding
-                </p>
-            </div>
-        </div>
-        <div class="col-sm-6">
-            <div class="main-post">
-                <h3 class="post-title">This is our main Post Title</h3>
-                <span class="post-author"><i class="fa-regular fa-user"></i> Yassine</span>
-                <span class="post-date"><i class="fa-regular fa-calendar"></i> 23/07/2022</span>
-                <span class="post-comments"><i class="fa-regular fa-comment"></i> 20 comments</span>
-                <img class="img-responsive img-thumbnail" src="https://source.unsplash.com/random/600x200/?img=4" alt="">
-                <p class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sed hic consectetur magnam omnis? Quae quo cupiditate facere, voluptatem praesentium cum labore, ipsam repellat culpa necessitatibus atque. Distinctio officia beatae atque, quasi ipsam blanditiis, neque provident dicta, quod veniam dignissimos autem cumque minus quae vel dolor illum voluptatibus iste aut?</p>
-                <hr>
-                <p class="categories">
-                <i class="fa-solid fa-tags"></i>                    
-                    Html, Test, Coding
-                </p>
-            </div>
-        </div>
+        <?php 
+            if (have_posts()) { //Check if there's a post
+                while(have_posts()){ //Loop Throught Posts
+                    the_post(); ?>
+                    <div class="col-sm-6">
+                        <div class="main-post">
+                            <h3 class="post-title">
+                                 <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                            </h3>
+                            <span class="post-author"><i class="fa-regular fa-user"></i> <?php  the_author_posts_link(); ?></span>
+                            <span class="post-date"><i class="fa-regular fa-calendar"></i> <?php the_time('F jS, Y'); ?></span>
+                            <span class="post-comments"><i class="fa-regular fa-comment"></i> <?php comments_popup_link('0 Comments','1 Comment','% Comments','comment-url','Comments Disabled') ?></span>
+                            <?php the_post_thumbnail( '', ['class' =>'img-fluid img-thumbnail rounded mx-auto d-block', 'title' => 'post image']); ?>
+                            <p class="post-content">
+                                <?php the_excerpt(); ?>
+                            </p>
+                            <hr>
+                            <p class="categories">
+                                <i class="fa-solid fa-tags"></i>                    
+                                        <?php the_category( ', ' ); ?>
+                            </p>
+                        </div>
+                    </div>       
+                  <?php        
+                } //End While Loop
+            } //End If Condition
+        ?>
     </div>
 </div>
-</div>
-
 
 <?php get_footer(); ?>
